a41240471a786a6f9aa157d711fc98d8d3abfe0a
wsu315 <wsu315@gmail.com>
2021-10-02 22:27:43 -0700

    spelling

--- format/format.CCLE.data.R
+++ format/format.CCLE.data.R
@@ -1,6 +1,6 @@
 ### DESCRIPTION ###################################################################################
 
-# Fromats data downloaded from DepMap. Removes unnecessary columns: depmapid, cell 
+# Formats data downloaded from DepMap. Removes unnecessary columns: depmapid, cell 
 # lineage information past cancer type. Saves the formatted data
 
 ### PREAMBLE ######################################################################################
@@ -9,22 +9,22 @@ setwd('/Users/wsu31/OneDrive/Desktop/R Stuff/linear-models/');
 library('tidyr');
 library('dplyr');
 
-### TITLE ##########################################################################################
+### TITLE #########################################################################################
 
 format.CCLE.data <- function(depmap.file.name, date, DepMap.data.type) {
     
-    ### LOAD DATA ###################################################################################
+    ### LOAD DATA #################################################################################
     depmap.data <- read.csv(file.path("raw-data", depmap.file.name));
    
-    ### FORMAT DATA ##################################################################################
+    ### FORMAT DATA ###############################################################################
     depmap.data <- subset(depmap.data, select = -c(depmap_id, lineage_2, lineage_3, lineage_4));
     
-    ### SAVE DATA ####################################################################################
+    ### SAVE DATA #################################################################################
     write.table(depmap.data, file.path("outputs", paste(date, DepMap.data.type, 'formatted.txt', sep = '-')), sep = '\t');
 };
 
-### DATA ANALYSIS ##################################################################################
+### DATA ANALYSIS #################################################################################
 
 format.CCLE.data('Drug_sensitivity_(PRISM_Repurposing_Primary_Screen)_19Q4_subsetted.csv', '2021.10.02', 'drug.sensitivity.seven.ovarian');
 
-### END ############################################################################################
+### END ###########################################################################################
